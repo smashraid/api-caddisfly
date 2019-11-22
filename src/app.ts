74c169afb31c935a79af78f53feb70bc4dc1cd63
STsuchida <Saulo.Tsuchida@cignium.com>
2019-11-22 14:48:24 -0500

    Prepare Heroku Deploy

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, {Application} from "express";
+//import bodyParser from "body-parser";
+
+class App {
+
+    public app: Application;
+
+    constructor() {
+        this.app = express();
+        this.config();        
+    }
+
+    private config(): void{
+        // support application/json type post data
+        //this.app.use(bodyParser.json());
+        //support application/x-www-form-urlencoded post data
+        //this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+}
+
+export default new App().app;
